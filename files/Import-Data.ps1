94aeef5613ae559d7f4f16cc5476251ecaa02eab
Tomilov Alexander <sopholos@sopholos.ru>
2024-06-07 19:51:41 +0500

    INFRA-230 import/export scripts

---
+++ files/Import-Data.ps1
@@ -0,0 +1,26 @@
+#!/snap/bin/powershell -Command
+# Ansible managed
+param(
+    [parameter(Mandatory=$false)][string]$destination = "/mnt/temp/superset/project",
+    [parameter(Mandatory=$false)][string]$SUPERSET_HOME = "/opt/superset",
+    [parameter(Mandatory=$false)][string]$SUPERSET_CONFIG_PATH = "/etc/superset/superset_config.py",
+    [parameter(Mandatory=$false)][string]$FLASK_APP = "superset"
+)
+
+$ErrorActionPreference = "Stop"
+$start = Get-Date
+
+try {
+    $env:SUPERSET_HOME = $SUPERSET_HOME
+    $env:SUPERSET_CONFIG_PATH = $SUPERSET_CONFIG_PATH
+    $env:FLASK_APP = $FLASK_APP
+
+    Set-Location $SUPERSET_HOME/bin;
+
+    ./superset import-dashboards --path $destination/dashboards.json
+    if ($LASTEXITCODE -ne 0) { throw "import-dashboards exited with code $LASTEXITCODE." }
+    Write-Host -ForegroundColor Green "Dashboards imported successfully"
+}
+finally {
+	Write-Host "Took: " ((Get-Date) - $start)
+}
